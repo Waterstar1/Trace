7b833e18eb7747f305984f5e24e15204002761dc
Waterstar1 <waterstar12001@gmail.com>
2020-11-08 00:53:01 -0800

    Firebase functions

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,9 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import * as Location from 'expo-location';
 import * as Device from 'expo-device';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-//dependencies firebase, expo-location, expo-device
+//dependencies firebase, expo-location, expo-device 
 
 const HomeScreen = () => {
   const firestore = firebase.firestore();
@@ -13,7 +14,7 @@ const HomeScreen = () => {
   const locationRef = firestore.collection('locations');
 
 
-  const onPress = async(e) => {
+  const trackLocation = async(e) => {
     console.log("Success");
     Location.requestPermissionsAsync();
     var lat = 0;
@@ -36,12 +37,12 @@ const HomeScreen = () => {
       covid: false
     })
 
-    let response = await fetch('https://3f882b53.us-south.apigw.appdomain.cloud/random-numbers');
-    let responseJson = await response.json();
-    console.log(responseJson[0]);
+    // let response = await fetch('https://3f882b53.us-south.apigw.appdomain.cloud/random-numbers');
+    // let responseJson = await response.json();
+    // console.log(responseJson[0]);
   }
 
-  const updateCovid = async(e) => {
+  const haveCovid = async(e) => {
     console.log("Success");
     const collection = firestore.collection("locations")
     const newDocumentBody = {
@@ -60,6 +61,27 @@ const HomeScreen = () => {
     })
   }
 
+  const comparesLocations = () => {
+    const haversine = require('haversine');
+    var nearby = [];
+    firestore.collection('locations').where('covid', '==', true).limit(2).get().then(snapshot => {
+      firestore.collection('locations').where('uid', '==', Device.osBuildId).get().then(snapshot2 => {
+        snapshot.docs.forEach(doc1 => {
+          snapshot2.docs.forEach(doc2 => {
+            let start = {latitude: doc1.data().location.latitude, longitude: doc1.data().location.longitude}
+            let end = {latitude: doc2.data().location.latitude, longitude: doc2.data().location.longitude}
+            if (haversine(start, end, {threshold: 100, unit: 'meter'})) {
+              nearby.push(doc1.data().location);
+            }
+          })
+        })
+      console.log(nearby);
+      })   
+    })
+  }
+
+
+
   // const alerts = async(e) => {
   //   const collection = firestore.collection("locations")
     
@@ -71,34 +93,32 @@ const HomeScreen = () => {
   //   }) 
   // }
 
-  const checks = async(e) => {
-    const haversine = require('haversine');
-    const collection = firestore.collection("locations");
-    console.log("HELLO");
-    try {
-      collection.where('uid', '==', "2").get().then(my_response => {
-        console.log("PASS");
-        console.log(my_response.docs[0].data());
-        collection.where('covid', '==', true).get().then(covid_response => {
-          var nearby = [];
-          console.log("Nearby:");
-          for (my_location of my_response.docs.data()) {
-            console.log("Nearby:");
-            for (other_location of covid_response.docs.data()) {
-              let start = {latitude: my_location.location.latitude, longitude: my_location.location.longitude}
-              let end = {latitude: other_location.location.latitude, longitude: other_location.location.longitude}
-              if (haversine(start, end, {threshold: 100, unit: 'meter'})) {
-                nearby.push(other_location);
-              }
-            }
-          }
-          console.log(nearby);
-        })
-      }) 
-    } catch {
-      console.log("fail");
-    }
-  }
+  // const checks = async(e) => {
+  //   const haversine = require('haversine');
+  //   const collection = firestore.collection("locations");
+  
+  //   try {
+  //     collection.where('uid', '==', "2").get().then(my_response => {
+  //       collection.where('covid', '==', true).get().then(covid_response => {
+  //         var nearby = [];
+  //         my_response.docs.forEach(my_location => {
+  //           console.log("TEST")
+  //           console.log(my_location.getdata())
+  //           covid_response.docs.forEach(other_location => { 
+  //             let start = {latitude: my_location.data().location.latitude, longitude: my_location.data().location.longitude}
+  //             let end = {latitude: other_location.data().location.latitude, longitude: other_location.data().location.longitude}
+  //             if (haversine(start, end, {threshold: 100, unit: 'meter'})) {
+  //               nearby.push(other_location);
+  //             }
+  //           })
+  //         })
+  //         //console.log(nearby);
+  //       })
+  //     }) 
+  //   } catch {
+  //     console.log("fail");
+  //   }
+  // }
 
   //function distance()
 
@@ -110,7 +130,7 @@ const HomeScreen = () => {
     <SafeAreaView style={styles.container}>
       <TouchableOpacity 
         style={styles.button}
-        onPress={checks}
+        //onPress={checks}
       >
         <Text style={styles.text}>
           Track
@@ -124,7 +144,7 @@ const HomeScreen = () => {
 
       <TouchableOpacity 
         style={styles.button}
-        onPress={updateCovid}
+        //onPress={updateCovid}
       >
         <Text style={styles.text}>
           Covid
